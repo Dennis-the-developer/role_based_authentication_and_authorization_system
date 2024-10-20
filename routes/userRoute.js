603d87c86b94bd893e9cc10e0c7b210ac078cfb2
Dennis Appah <dev.dennisappah@gmail.com>
2024-10-20 12:01:19 +0000

    added discord redirect route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,6 +7,7 @@ const userRouter = Router();
 
 userRouter.post('/auth/register', isAuthenticated, hasPermission("createUser"), Register);
 userRouter.post('/auth/login', isAuthenticated, Login);
+userRouter.post('/auth/oauth/dicord/redirect', Login);
 userRouter.post('/auth/assign-role', isAuthenticated, hasPermission("assignRole"), AssignRole);
 userRouter.delete('/user/:id', isAuthenticated, hasPermission("deleteUser"), DeleteUser);
 userRouter.get('/public-data', ViewPublicData);
