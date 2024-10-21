3e0925337ab227e48f88376f6da44c06d60637f3
Dennis Appah <dev.dennisappah@gmail.com>
2024-10-21 16:39:03 +0000

    added admin route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import passport from "passport";
 import "../strategies/discordStrategy.js";
-import { Register, Login, AssignRole, DeleteUser, ViewPublicData } from "../controllers/userController.js";
+import { CreateAdmin, Register, Login, AssignRole, DeleteUser, ViewPublicData } from "../controllers/userController.js";
 import { isAuthenticated } from "../middlewares/authn.js";
 import { hasPermission } from "../middlewares/authz.js";
 
 const userRouter = Router();
 
+userRouter.post('/auth/create-admin', CreateAdmin);
 userRouter.post('/auth/register', isAuthenticated, hasPermission("createUser"), Register);
 userRouter.post('/auth/login', isAuthenticated, Login);
 userRouter.get('/auth/oauth/discord-authorise', passport.authenticate('discord'));
